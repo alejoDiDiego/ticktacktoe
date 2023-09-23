8f77125e54f9e89d711452263e699c6d937301a4
alejoDiDiego <alejoh2014@gmail.com>
2023-09-23 04:57:04 -0300

    finished?

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,8 +2,10 @@ import Board from "@/components/Board";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center  pt-24">
-      <h1>Tik tak toe</h1>
+    <main
+      className={`bg-slate-300 flex min-h-screen flex-col items-center pt-24`}
+    >
+      <h1 className="font-extrabold text-6xl text-center">Tik tak toe</h1>
       <Board />
     </main>
   );
--- frontend/src/components/Board.tsx
+++ frontend/src/components/Board.tsx
@@ -90,27 +90,31 @@ const Board = () => {
   // "3" | "4" | "5"
   // "6" | "7" | "8"
   return (
-    <div>
-      <p>
-        {playerOneWins} vs {playerTwoWins}
+    <div className="flex flex-col items-center mt-8 gap-4">
+      <p className="text-4xl font-bold">
+        <span className="text-purple-600">{playerOneWins}</span> vs{" "}
+        <span className="text-blue-600">{playerTwoWins}</span>
       </p>
 
-      <button onClick={restartGame} className="bg-blue-400 p-2">
+      <button
+        onClick={restartGame}
+        className="bg-teal-500 p-2 px-3 rounded-md shadow-md text-2xl text-white cursor-pointer transition-all hover:bg-teal-600"
+      >
         Restart
       </button>
 
-      <div className="w-[500px] h-[500px] bg-gray-800 shadow-2xl shadow-black grid grid-cols-3 grid-rows-3 p-5 text-white gap-4 rounded-2xl">
+      <div className="w-[500px] h-[500px] bg-gray-800 shadow-lg shadow-black grid grid-cols-3 grid-rows-3 p-5 text-white gap-4 rounded-2xl">
         <div
           onClick={() => setGrid(0)}
-          className={`border-2 border-gray-50 cursor-pointer rounded-2xl  w-full h-full transition-all duration-300 flex items-center justify-center ${
+          className={`border-2 border-gray-50 cursor-pointer rounded-2xl  w-full h-full transition-all flex items-center justify-center duration-300 ${
             game[0].length == 0
-              ? "bg-transparent"
+              ? "bg-transparent duration-200 hover:bg-gray-500"
               : game[0] == "x"
               ? "bg-purple-600 shadow-lg shadow-purple-600"
               : "bg-blue-600 shadow-lg shadow-blue-600"
           }`}
         >
-          <p
+          <div
             className={`transition-all duration-300  ${
               game[0].length > 0 ? "text-white" : "text-transparent"
             }`}
@@ -118,20 +122,20 @@ const Board = () => {
             <p className="text-8xl flex items-center justify-center m-0 pb-3">
               {game[0]}
             </p>
-          </p>
+          </div>
         </div>
 
         <div
           onClick={() => setGrid(1)}
           className={`border-2 border-gray-50 cursor-pointer rounded-2xl  w-full h-full transition-all duration-300 flex items-center justify-center ${
             game[1].length == 0
-              ? "bg-transparent"
+              ? "bg-transparent duration-200 hover:bg-gray-500"
               : game[1] == "x"
               ? "bg-purple-600 shadow-lg shadow-purple-600"
               : "bg-blue-600 shadow-lg shadow-blue-600"
           }`}
         >
-          <p
+          <div
             className={`transition-all duration-300  ${
               game[1].length > 0 ? "text-white" : "text-transparent"
             }`}
@@ -139,20 +143,20 @@ const Board = () => {
             <p className="text-8xl flex items-center justify-center m-0 pb-3">
               {game[1]}
             </p>
-          </p>
+          </div>
         </div>
 
         <div
           onClick={() => setGrid(2)}
           className={`border-2 border-gray-50 cursor-pointer rounded-2xl  w-full h-full transition-all duration-300 flex items-center justify-center ${
             game[2].length == 0
-              ? "bg-transparent"
+              ? "bg-transparent duration-200 hover:bg-gray-500"
               : game[2] == "x"
               ? "bg-purple-600 shadow-lg shadow-purple-600"
               : "bg-blue-600 shadow-lg shadow-blue-600"
           }`}
         >
-          <p
+          <div
             className={`transition-all duration-300  ${
               game[2].length > 0 ? "text-white" : "text-transparent"
             }`}
@@ -160,20 +164,20 @@ const Board = () => {
             <p className="text-8xl flex items-center justify-center m-0 pb-3">
               {game[2]}
             </p>
-          </p>
+          </div>
         </div>
 
         <div
           onClick={() => setGrid(3)}
           className={`border-2 border-gray-50 cursor-pointer rounded-2xl  w-full h-full transition-all duration-300 flex items-center justify-center ${
             game[3].length == 0
-              ? "bg-transparent"
+              ? "bg-transparent duration-200 hover:bg-gray-500"
               : game[3] == "x"
               ? "bg-purple-600 shadow-lg shadow-purple-600"
               : "bg-blue-600 shadow-lg shadow-blue-600"
           }`}
         >
-          <p
+          <div
             className={`transition-all duration-300  ${
               game[3].length > 0 ? "text-white" : "text-transparent"
             }`}
@@ -181,20 +185,20 @@ const Board = () => {
             <p className="text-8xl flex items-center justify-center m-0 pb-3">
               {game[3]}
             </p>
-          </p>
+          </div>
         </div>
 
         <div
           onClick={() => setGrid(4)}
           className={`border-2 border-gray-50 cursor-pointer rounded-2xl  w-full h-full transition-all duration-300 flex items-center justify-center ${
             game[4].length == 0
-              ? "bg-transparent"
+              ? "bg-transparent duration-200 hover:bg-gray-500"
               : game[4] == "x"
               ? "bg-purple-600 shadow-lg shadow-purple-600"
               : "bg-blue-600 shadow-lg shadow-blue-600"
           }`}
         >
-          <p
+          <div
             className={`transition-all duration-300  ${
               game[4].length > 0 ? "text-white" : "text-transparent"
             }`}
@@ -202,20 +206,20 @@ const Board = () => {
             <p className="text-8xl flex items-center justify-center m-0 pb-3">
               {game[4]}
             </p>
-          </p>
+          </div>
         </div>
 
         <div
           onClick={() => setGrid(5)}
           className={`border-2 border-gray-50 cursor-pointer rounded-2xl  w-full h-full transition-all duration-300 flex items-center justify-center ${
             game[5].length == 0
-              ? "bg-transparent"
+              ? "bg-transparent duration-200 hover:bg-gray-500"
               : game[5] == "x"
               ? "bg-purple-600 shadow-lg shadow-purple-600"
               : "bg-blue-600 shadow-lg shadow-blue-600"
           }`}
         >
-          <p
+          <div
             className={`transition-all duration-300  ${
               game[5].length > 0 ? "text-white" : "text-transparent"
             }`}
@@ -223,20 +227,20 @@ const Board = () => {
             <p className="text-8xl flex items-center justify-center m-0 pb-3">
               {game[5]}
             </p>
-          </p>
+          </div>
         </div>
 
         <div
           onClick={() => setGrid(6)}
           className={`border-2 border-gray-50 cursor-pointer rounded-2xl  w-full h-full transition-all duration-300 flex items-center justify-center ${
             game[6].length == 0
-              ? "bg-transparent"
+              ? "bg-transparent duration-200 hover:bg-gray-500"
               : game[6] == "x"
               ? "bg-purple-600 shadow-lg shadow-purple-600"
               : "bg-blue-600 shadow-lg shadow-blue-600"
           }`}
         >
-          <p
+          <div
             className={`transition-all duration-300  ${
               game[6].length > 0 ? "text-white" : "text-transparent"
             }`}
@@ -244,20 +248,20 @@ const Board = () => {
             <p className="text-8xl flex items-center justify-center m-0 pb-3">
               {game[6]}
             </p>
-          </p>
+          </div>
         </div>
 
         <div
           onClick={() => setGrid(7)}
           className={`border-2 border-gray-50 cursor-pointer rounded-2xl  w-full h-full transition-all duration-300 flex items-center justify-center ${
             game[7].length == 0
-              ? "bg-transparent"
+              ? "bg-transparent duration-200 hover:bg-gray-500"
               : game[7] == "x"
               ? "bg-purple-600 shadow-lg shadow-purple-600"
               : "bg-blue-600 shadow-lg shadow-blue-600"
           }`}
         >
-          <p
+          <div
             className={`transition-all duration-300  ${
               game[7].length > 0 ? "text-white" : "text-transparent"
             }`}
@@ -265,14 +269,14 @@ const Board = () => {
             <p className="text-8xl flex items-center justify-center m-0 pb-3">
               {game[7]}
             </p>
-          </p>
+          </div>
         </div>
 
         <div
           onClick={() => setGrid(8)}
           className={`border-2 border-gray-50 cursor-pointer rounded-2xl  w-full h-full transition-all duration-300 flex items-center justify-center ${
             game[8].length == 0
-              ? "bg-transparent"
+              ? "bg-transparent duration-200 hover:bg-gray-500"
               : game[8] == "x"
               ? "bg-purple-600 shadow-lg shadow-purple-600"
               : "bg-blue-600 shadow-lg shadow-blue-600"
@@ -290,17 +294,23 @@ const Board = () => {
         </div>
       </div>
       {winner > 0 ? (
-        <div>
-          <p>The winner is the player {winner}</p>
-          <button onClick={continueGame} className="bg-blue-400 p-2">
+        <div className="flex flex-col justify-center items-center">
+          <p className="text-xl mt-1 mb-2">The winner is the player {winner}</p>
+          <button
+            onClick={continueGame}
+            className="bg-blue-400 p-2 px-3 rounded-md shadow-md text-2xl text-white cursor-pointer transition-all hover:bg-blue-600"
+          >
             Continue
           </button>
         </div>
       ) : null}
       {continueB ? (
-        <div>
-          <p>It's a tie!</p>
-          <button onClick={continueGame} className="bg-blue-400 p-2">
+        <div className="flex flex-col justify-center items-center">
+          <p className="text-xl mt-1 mb-2">It's a tie!</p>
+          <button
+            onClick={continueGame}
+            className="bg-blue-400 p-2 px-3 rounded-md shadow-md text-2xl text-white cursor-pointer transition-all hover:bg-blue-600"
+          >
             Continue
           </button>
         </div>
