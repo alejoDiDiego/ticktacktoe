85395430a573a137d1133a66b56033b60950ecf7
Alejo Di Diego <93689456+alejoDiDiego@users.noreply.github.com>
2023-09-29 17:28:16 -0300

    Update page.tsx

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main
       className={`bg-slate-300 flex min-h-screen flex-col items-center pt-24`}
     >
-      <h1 className="font-extrabold text-6xl text-center">Tik tak toe</h1>
+      <h1 className="font-extrabold text-6xl text-center">Ti tac toe</h1>
       <Board />
     </main>
   );
